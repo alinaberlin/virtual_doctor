eea4f79e2d793ccd0ea5bbd9e737b7a1506697b1
Alina Ghetler <alina.ghetler@gmail.com>
2020-08-24 23:01:38 +0200

    Add invoice creation route

--- src/services/session-service.js
+++ src/services/session-service.js
@@ -1,10 +1,7 @@
-const Session = require('../models/session')
-
-const calculateSessionPrice = async (sessionId, pricePerMinute) => {
-    const session = await Session.findById(sessionId);
-    const appointment = { ...session.appointment };
-    const price = appointment.duration * pricePerMinute;
-    return price;
+const calculateSessionPrice = (session, pricePerMinute) => {
+  const appointment = { ...session.appointment }
+  const price = appointment.duration * pricePerMinute
+  return price
 }
 
 module.exports = { calculateSessionPrice }
