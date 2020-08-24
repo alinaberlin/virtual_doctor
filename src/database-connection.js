2ea5c1af2ce4f5c4032e5c8211347fbd3be07c83
Alina Ghetler <alina.ghetler@gmail.com>
2020-08-24 18:48:38 +0200

    Fix docker-compose.yml

--- src/database-connection.js
+++ src/database-connection.js
@@ -4,9 +4,24 @@ const username = process.env.MONGODB_USERNAME
 const password = process.env.MONGODB_PASSWORD
 const dbName = process.env.MONGODB_DATABASE
 const dbHost = process.env.MONGODB_HOST
+let connectionString = process.env.MONGODB_CONNECTION_STRING
 
 mongoose.set('debug', true)
-const connectionString = process.env.MONGODB_REMOTE ? `mongodb://${dbHost}/${dbName}?retryWrites=true&w=majority` : 'mongodb://localhost:'
+if (!connectionString) {
+  connectionString = process.env.MONGODB_REMOTE
+    ? `mongodb://${dbHost}/${dbName}?retryWrites=true&w=majority`
+    : 'mongodb://localhost:'
+}
+console.log('Connection is', connectionString)
+
+mongoose.set('debug', true)
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connection established'))
+  .catch(console.log)
 
 /* mongoose
   .connect(
