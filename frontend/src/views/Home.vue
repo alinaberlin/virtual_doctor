f87f59804a34dbec14abd0a0b88c6a1e5bab3fcd
Alina Ghetler <alina.ghetler@gmail.com>
2020-08-26 23:17:28 +0200

    Remove the hello-world component from project

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,20 +1,14 @@
 <template lang="pug">
   .home
-    img(alt="Vue logo" src="../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
     div(v-for="user in users") {{ user.firstName }}
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/hello-world.vue'
 import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  },
+  components: {},
   data() {
     return {
       users: []
