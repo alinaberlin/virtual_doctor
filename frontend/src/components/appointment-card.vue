142ed1dacb749d863b964a5d84f1cd3e32036d1f
Alina Ghetler <alina.ghetler@gmail.com>
2020-08-29 22:52:47 +0200

    Create appoitment-card

--- frontend/src/components/appointment-card.vue
+++ frontend/src/components/appointment-card.vue
@@ -1,11 +1,20 @@
 <script>
 export default {
   name: 'Appointment',
-  props: ['appointment'],
+  props: ['appointment']
 }
 </script>
 
 <template lang="pug">
 .box
-  h1 {{name }} 
+  h1 {{name }}
 </template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid #333;
+  background: #dd0;
+  border-radius: 0.3rem;
+}
+</style>
