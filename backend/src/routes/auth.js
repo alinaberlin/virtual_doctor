a96b47279b7c6ac82fb36c9fa9efcbe94d26453d
Alina Ghetler <alina.ghetler@gmail.com>
2020-09-22 19:41:57 +0200

    Handle better login process

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,6 +4,21 @@ const router = express.Router()
 const passport = require('passport')
 const User = require('../models/user')
 
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err)
+        }
+
+        if (!user) {
+            return res.status(400).send([user, 'Cannot log in', info])
+        }
+
+        req.login(user, err => {
+            res.send(user)
+        })
+    })(req, res, next)
+})
 
 router.post('/register', (req, res, next) => {
     User.register({ username: req.body.firstName }, req.body.password, (err, response) => {
