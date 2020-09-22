1c7a4fec4d973685141cd0a5ef3fb567d514548a
Alina Ghetler <alina.ghetler@gmail.com>
2020-09-22 19:40:35 +0200

    Import local strategy

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,19 +4,19 @@ const router = express.Router()
 const passport = require('passport')
 const User = require('../models/user')
 
-router.get('/login', (req, res, next) => {
-    res.render('login')
-})
 
 router.post('/register', (req, res, next) => {
     User.register({ username: req.body.firstName }, req.body.password, (err, response) => {
-    res.send('OK')
-})
+        res.send('OK')
+    })
 })
 
-router.post('/local', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: 'auth/login'
-}))
+router.post(
+    '/local',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: 'auth/login',
+    })
+)
 
 module.exports = router
--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,3 +1,4 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
